53df4cd4b83e435e397b9b570131e122628b5b2c
Prince Kaizen Namwali <33nano@pm.me>
2021-02-14 06:04:50 +0000

    Create weird.sh

---
+++ weird.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "I was bourne again, only to be devoured by a fish and a shell."
+
+echo "You are the neofetch of my soul and the powerline that holds me in control"
+
+echo "Cowsay is silent when man touch mein.conf"
+
