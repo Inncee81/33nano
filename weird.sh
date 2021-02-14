37c0c215d9cdce4aa3e9097212edb1d6ff80fdf5
Prince Kaizen Namwali <33nano@pm.me>
2021-02-14 07:08:54 +0000

    Update weird.sh

--- weird.sh
+++ weird.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 name="Kaizen"
+dream="GameDev"
 
 echo Hello, ${name}.  "I was bourne again, only to be devoured by a fish and a shell."
 
 echo "You are the neofetch of my soul and the powerline that holds me in control"
 
-echo "Cowsay is silent when man touch mein.conf"
+echo "Cowsay told me i have what it take to become a ${dream} "
 
 
