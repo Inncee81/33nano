a25ff54799864ec752935ee4abb97867f94b0aaf
Prince Kaizen Namwali <33nano@pm.me>
2021-02-14 07:06:49 +0000

    Update weird.sh

--- weird.sh
+++ weird.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-echo "I was bourne again, only to be devoured by a fish and a shell."
+name="Kaizen"
+
+echo Hello, ${name}.  "I was bourne again, only to be devoured by a fish and a shell."
 
 echo "You are the neofetch of my soul and the powerline that holds me in control"
 
 echo "Cowsay is silent when man touch mein.conf"
+
 
